2561c0977a65a9e2097659886751efe0900153df
SimoneZampieri <simonezampieri106@gmail.com>
2024-12-03 16:57:12 +0100

    [end] consegna

--- JS/script.js
+++ JS/script.js
@@ -4,16 +4,45 @@
 //costanti
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
+const mail = [];
 //procedimento
+const fetchEmails = () =>{
 
-for(let i = 0; i < 10; i++){
+    const promises = [];
     
-    let mail = [];
+    for(let i = 0; i < 10; i++){
+        
+    
+        const promise = fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+            mail.push(data.response)
+        })
 
-    fetch(endpoint)
-    .then(response => response.json())
-    .then(data => {
-        mail.push(data.response)
+        promises.push(promise)
+    }
+
+    Promise.all(promises)
+    .then(() => {
+        displayEmails();
     })
+    .catch(error => {
+        console.error('Errore:', error);
+    });
+};
 
-}
+
+const displayEmails = () => {
+    const mailCont = document.getElementById('email-list')
+
+    mail.forEach(email => {
+        const li = document.createElement('li');
+        li.textContent = email;
+        mailCont.appendChild(li)
+    });
+};
+
+
+//chiamata funzione
+
+fetchEmails()
