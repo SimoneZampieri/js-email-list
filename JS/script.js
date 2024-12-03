f493849c5d14e64858537441280a7357003133cb
SimoneZampieri <simonezampieri106@gmail.com>
2024-12-03 16:07:16 +0100

    [fix] js file

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,19 @@
 //attraverso l'API fornita generare 10 indirizzi email e stamparli in pagina 
 
-// Funzione per ottenere 10 email randomiche usando axios
-  async function generateRandomEmails() {
-    const emailListElement = document.getElementById('email-list');
 
-    for (let i = 0; i < 10; i++) {
-        try {
-            // Chiamata API per ottenere una singola email randomica
-            const response = await axios.get('https://flynn.boolean.careers/exercises/api/random/mail');
-            
-            // Aggiungi l'email alla lista HTML
-            const listItem = document.createElement('li');
-            listItem.textContent = response.data.response;  // L'email si trova in response.data.response
-            emailListElement.appendChild(listItem);
-        } catch (error) {
-            console.error('Errore nella richiesta:', error);
-        }
-    }
+//costanti
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+//procedimento
+
+for(let i = 0; i < 10; i++){
+    
+    let mail = [];
+
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+        mail.push(data.response)
+    })
+
 }
-
-// Chiamata alla funzione per generare le email
-generateRandomEmails();
